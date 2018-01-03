ce933ebcf7cc23410796ab0efde9de432ea287ee
DaisyHax <dishi.sarthak@gmail.com>
2018-01-03 11:21:42 +0530

    first commit

--- backup.py
+++ backup.py
@@ -12,25 +12,26 @@ r = requests.get('http://127.0.0.1:8000/pflege/')
 # extracting data in json format
 data = r.json()
 
-# print(data)
+print(data)
 
 for i in data:
-    # print(i)
+    print(i)
     idee=i['id']
     cn=i['time']
     dt=i['date']
-    ctt=i['content']
-    # print(cn)
-    # print(dt)
+    to=i['reciever']
+    content=i['content']
+    print(cn)
+    print(dt)
     break
 
 
 s1=datetime.now().strftime('%Y-%m-%d %H:%M')
 s2=dt+" "+cn
-print("System Time "+s1)
-print("Send Time "+s2)
+print(s1)
+print(s2)
 if(s1==s2):
-    # print("Done")
+    print("Done")
     #requests.delete('http://127.0.0.1:8000/Pflege/?ide='+idee)
     usr = "bhalotia.tannu@gmail.com"
     pwd = "bhagalpur"
@@ -48,6 +49,6 @@ if(s1==s2):
     time.sleep(5)
     textAreaElem = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div/div[2]/span/div[2]/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div/div[2]/div/div/div/div')
     textAreaElem.click()
-    textAreaElem.send_keys(ctt)
+    textAreaElem.send_keys(content)
     textAreaElem.send_keys(Keys.RETURN)
     time.sleep(1000)
